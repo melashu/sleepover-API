8002d896b289c6c14d45262653589ac1e107eb43
melashu <meshu102@outlook.com>
2023-01-20 13:09:37 +0300

    Add basic routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :rooms
+      # Put your route below 
+      # 
+      
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    end
+  end
 end
