8ab80bc00a1f26fd6c181cf1bd1931d969498c28
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-01-20 19:59:23 +0300

    Fix linters

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,38 +1,31 @@
 class ReservationsController < ApplicationController
-
-  before_action :set_author
-
   def index
-   @reservations= Reservation.all.where(user_id: current_user.id)
-  end  
+    @reservations = Reservation.all.where(user_id: current_user.id)
+  end
 
   def new
-   @reservation =  Reservation.new
-  end  
+    @reservation = Reservation.new
+  end
 
   def create
-    reservation =  Reservation.new(reservation_params)
+    reservation = Reservation.new(reservation_params)
     if reservation.save
-        redirect_to root_path, notice: 'Reservation was successfully created.'
+      redirect_to root_path, notice: 'Reservation was successfully created.'
     else
-        render :new, status: 422
+      render :new, status: 422
     end
   end
 
   def destroy
     reservation = Reservation.find(params[:id])
-    reservation.destroy
-        redirect_to root_path, notice: 'Reservation was successfully deleted.'
-    end
+    return unless reservation.destroy
+
+    redirect_to root_path, notice: 'Reservation was successfully deleted.'
   end
 
   private
 
-  def set_author
-    @author = current_user
-  end
-
   def reservation_params
     params.require(:reservation).permit(:start_date, :end_date, :user_id, :room_id)
-  end  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :rooms
-      # Put your route below 
-      # 
-      
-
+      # Put your route below
+      #
     end
   end
 end
