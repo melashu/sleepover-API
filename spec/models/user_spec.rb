4f0f893e5eba21f4124b4b021ef5296c60d9a3fd
benjamin kisenge <benkisenge03@gmail.com>
2023-01-24 12:58:51 +0200

    Generate room rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Tom', email: 'tom00@gmail.com', password: 'tom244', password_digest: 'tom244' ) }
+  subject { User.new(name: 'Tom baraka',username: 'tom', email: 'tom00@gmail.com', password: 'tom244', password_digest: 'tom244' ) }
   before { subject.save }
   it 'Name must not be blank' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+    subject.name = 'Tom baraka'
+    expect(subject).to be_valid
   end
 
   it 'Email must not be blank' do
