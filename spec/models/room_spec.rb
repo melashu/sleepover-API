29836a943085069d09f5e3142167794536e3110c
benjamin kisenge <benkisenge03@gmail.com>
2023-01-24 13:35:45 +0200

    Add test validation for room model

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:prices) }
+    it { should validate_presence_of(:number_of_bed) }
+    it { should validate_presence_of(:photo) }
+  end
 end
