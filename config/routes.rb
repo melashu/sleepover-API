7f690019eb95d287ece2b33b39c660b4e718859c
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-01-24 16:32:30 +0300

    Add routes for reservation and reservation history

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'rooms/reservation', to: 'rooms#checkout_reservation'
-
+      get "reservation/history", to: 'reservation#history'
       resources :hotels
       resources :rooms
+      resources :reservation
 
       resources :users
       end
