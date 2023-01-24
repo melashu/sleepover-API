262f15e7e49e9c7e1712899f1d76eb037ff5a7f2
benjamin kisenge <benkisenge03@gmail.com>
2023-01-24 12:24:14 +0200

    Add example for name

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { User.new(name: 'Tom', email: 'tom00@gmail.com', password: 'tom244', password_digest: 'tom244' ) }
+  before { subject.save }
+  it 'Name must not be blank' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+end
