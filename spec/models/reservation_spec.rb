c7363c8ae092ffb382440f27f1c0fd0bf355b0b0
benjamin kisenge <benkisenge03@gmail.com>
2023-01-24 14:05:17 +0200

    Add Associations test for reservations

---
+++ spec/models/reservation_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Reservation, type: :model do
+  describe 'Associations ' do
+    it { should belong_to(:user)}
+    it { should belong_to(:room)}
+  end
+end
