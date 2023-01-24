675662b7889906d0cc6f9afcf19bdf67cdf0e7ef
benjamin kisenge <benkisenge03@gmail.com>
2023-01-24 14:13:40 +0200

    Add test association to hotel model

--- spec/models/hotel_spec.rb
+++ spec/models/hotel_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Hotel, type: :model do
     it { should have_one_attached(:image)}
   end
 
+  describe 'Associtions' do
+    it { should have_many(:rooms) }
+    it { should belong_to(:user)}
+  end
 end
