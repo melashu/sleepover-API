af8817aa1ca59b8a2d4eba355cd13acb7de88e7d
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-01-24 16:30:19 +0300

    Add update reservation method

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,11 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  after_create :update_reservation
+
+  private
+  def update_reservation
+    room.update(reserve: true)
+  end
 end
