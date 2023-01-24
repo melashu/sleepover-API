533bea1426d498f036395707d0165dd8a63c0cd0
benjamin kisenge <benkisenge03@gmail.com>
2023-01-24 13:26:32 +0200

    Add test for validation sake

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,8 @@ RSpec.describe User, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
   end
 end
