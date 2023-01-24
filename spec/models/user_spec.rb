278dff3c3e8c9918d3ebdd691a0e738003a8a516
benjamin kisenge <benkisenge03@gmail.com>
2023-01-24 13:32:51 +0200

    Add association test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,4 +34,10 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
   end
+
+  describe 'asscociations' do
+    it { should have_many(:rooms).through(:reservations) }
+    it { should have_many(:hotels) }
+    it { should have_many(:reservations) }
+  end
 end
