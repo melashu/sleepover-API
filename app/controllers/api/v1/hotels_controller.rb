982147a7c71da0ce72c4aa53692093fca0128e0d
melashu <meshu102@outlook.com>
2023-01-25 17:26:21 +0300

    Update hotels controller

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -1,18 +1,19 @@
 class Api::V1::HotelsController < ApplicationController
-  before_action :set_hotel, only: %i[destroy show]
-  def index
-    hotel = @current_user.hotels
+  before_action :check_hotel, only: %i[destroy show]
+  load_and_authorize_resource
 
-    render json: hotel if hotel.any?
-    render json: { message: 'empty' } unless hotel.any?
+  def index
+    hotel = Hotel.includes(:rooms).all.where(rooms: { reserve: false })
+    render json: hotel
   end
 
   def create
     hotel = Hotel.new(check_param)
+    hotel.user_id = @current_user.id
     if hotel.save
       render json: { message: 'success' }
     else
-      render json: { message: 'error', hotel: }
+      render json: { message: 'error', hotel: hotel.errors.full_messages }
     end
   end
 
@@ -20,21 +21,21 @@ class Api::V1::HotelsController < ApplicationController
     if @hotel.destroy
       render json: { message: 'success' }
     else
-      render json: { message: 'error' }
+      render json: { message: 'error', error: @hotel.errors.full_messages }
     end
   end
 
   def show
-    render json: { message: 'success', hotel: @hotel }
+    render json: @hotel
   end
 
   private
 
-  def set_hotel
-    @hotel = @current_user.hotels.find(params[:id])
+  def check_hotel
+    @hotel = Hotel.find(params[:id])
   end
 
   def check_param
-    params.permit(:name, :city, :phone, :user_id, :country, :image)
+    params.permit(:name, :city, :phone, :country, :image, :detail)
   end
 end
