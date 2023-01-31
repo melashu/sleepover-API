dbe3a3167830904d75c0c80fda2c30ceadd37fdf
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-01-31 23:24:12 +0300

    Fix issues

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
