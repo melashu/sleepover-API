2abc8273222ba9c766423d34fd530ded5ec4ea7f
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2023-02-02 16:28:45 +0300

    Update file for deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
 bundle exec rails db:migrate
 bundle exec rails db:seed
